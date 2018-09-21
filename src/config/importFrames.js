2b79f47af9fdbe15a38623074e31b9ca791b3214
Alexander Levin <alevin.ru@gmail.com>
2018-09-21 15:41:28 +0300

    parse frames xslx

---
+++ src/config/importFrames.js
@@ -0,0 +1,11 @@
+export default [
+  { title: 'Код', name: 'code' },
+  { title: 'Наименование', name: 'name' },
+  { title: 'Артикул', name: 'article' },
+  { title: 'Размер', name: 'size' },
+  { title: 'Мин. коробка', name: 'packageRel', type: 'int' },
+  { title: 'Вес ед., кг', name: 'weight', type: 'decimal' },
+  { title: 'Материал', name: 'material' },
+  { title: 'Прозрачная вставка', name: 'screen' },
+  { title: 'Крепление задника', name: 'back' },
+];
--- src/middleware/document.js
+++ src/middleware/document.js
@@ -1,5 +1,11 @@
 import log from 'sistemium-telegram/services/log';
 import bot from 'sistemium-telegram/services/bot';
+import axios from 'axios';
+import xlsx from 'node-xlsx';
+import trim from 'lodash/trim';
+import map from 'lodash/map';
+import filter from 'lodash/filter';
+import importFrames from '../config/importFrames';
 
 const { debug } = log('document');
 
@@ -32,8 +38,61 @@ export async function onGetFile(ctx) {
 
   debug('onGetFile', fileId);
 
-  const link = await bot.telegram.getFileLink(fileId);
+  const file = await bot.telegram.getFile(fileId);
 
-  await ctx.reply(link);
+  debug(file);
+
+  const url = await bot.telegram.getFileLink(fileId);
+  const xls = await getFile(url);
+
+  const sheet = xls[0].data;
+  const [titles] = sheet;
+  const sheetData = sheet.slice(1);
+
+  await ctx.replyWithHTML(`<b>${sheetData.length}</b> строк`);
+
+  const columns = importFrames.map(({ title, name, type }) => ({
+    type, name, idx: titles.indexOf(title), title,
+  }));
+
+  const missing = filter(columns, { idx: -1 });
+
+  if (missing.length) {
+    await ctx.replyWithHTML(`Не найдены колонки: <b>${map(missing, 'title').join(', ')}</b>`);
+    return;
+  }
+
+  const data = sheetData.map(row => {
+
+    const res = {};
+
+    columns.forEach(({ name, idx, type }) => {
+      const value = row[idx];
+      res[name] = type ? value : trim(value);
+    });
+
+    return res;
+
+  });
+
+  await ctx.replyWithHTML(`<pre>${JSON.stringify(data[0], null, 2)}</pre>`);
+
+}
+
+async function getFile(url) {
+
+  const response = await axios({
+    method: 'get',
+    responseType: 'arraybuffer',
+    url,
+    headers: {
+      'Content-Type': 'application/x-msexcel',
+    },
+  });
+
+  // debug(response.data);
+  return xlsx.parse(response.data, { type: 'buffer' });
+
+  // return xlsx.parse(response, { type: 'buffer' });
 
 }
