fb0638e9d604ddac0bfc4364897dcbd136ef056a
Alexander Levin <alevin.ru@gmail.com>
2018-09-21 16:27:25 +0300

    parseFramesFile

--- src/config/importFrames.js
+++ src/config/importFrames.js
@@ -3,7 +3,7 @@ export default [
   { title: 'Наименование', name: 'name' },
   { title: 'Артикул', name: 'article' },
   { title: 'Размер', name: 'size' },
-  { title: 'Мин. коробка', name: 'packageRel', type: 'int' },
+  { title: 'Мин.коробка', name: 'packageRel', type: 'int' },
   { title: 'Вес ед., кг', name: 'weight', type: 'decimal' },
   { title: 'Материал', name: 'material' },
   { title: 'Прозрачная вставка', name: 'screen' },
--- src/middleware/document.js
+++ src/middleware/document.js
@@ -38,43 +38,12 @@ export async function onGetFile(ctx) {
 
   debug('onGetFile', fileId);
 
-  const file = await bot.telegram.getFile(fileId);
-
-  debug(file);
-
   const url = await bot.telegram.getFileLink(fileId);
   const xls = await getFile(url);
 
-  const sheet = xls[0].data;
-  const [titles] = sheet;
-  const sheetData = sheet.slice(1);
+  const data = parseFramesFile(xls);
 
-  await ctx.replyWithHTML(`<b>${sheetData.length}</b> строк`);
-
-  const columns = importFrames.map(({ title, name, type }) => ({
-    type, name, idx: titles.indexOf(title), title,
-  }));
-
-  const missing = filter(columns, { idx: -1 });
-
-  if (missing.length) {
-    await ctx.replyWithHTML(`Не найдены колонки: <b>${map(missing, 'title').join(', ')}</b>`);
-    return;
-  }
-
-  const data = sheetData.map(row => {
-
-    const res = {};
-
-    columns.forEach(({ name, idx, type }) => {
-      const value = row[idx];
-      res[name] = type ? value : trim(value);
-    });
-
-    return res;
-
-  });
-
+  await ctx.replyWithHTML(`Имрортировано <b>${data.length}</b> строк`);
   await ctx.replyWithHTML(`<pre>${JSON.stringify(data[0], null, 2)}</pre>`);
 
 }
@@ -94,5 +63,37 @@ async function getFile(url) {
   return xlsx.parse(response.data, { type: 'buffer' });
 
   // return xlsx.parse(response, { type: 'buffer' });
+
+}
+
+
+function parseFramesFile(xls) {
+
+  const sheet = xls[0].data;
+  const [titles] = sheet;
+  const sheetData = sheet.slice(1);
+
+  const columns = importFrames.map(({ title, name, type }) => ({
+    type, name, idx: titles.indexOf(title), title,
+  }));
+
+  const missing = filter(columns, { idx: -1 });
+
+  if (missing.length) {
+    throw new Error(`Не найдены колонки: [${map(missing, 'title').join(', ')}]`);
+  }
+
+  return sheetData.map(row => {
+
+    const res = {};
+
+    columns.forEach(({ name, idx, type }) => {
+      const value = row[idx];
+      res[name] = type ? value : trim(value);
+    });
+
+    return res;
+
+  });
 
 }
