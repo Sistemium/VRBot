6e6a0b9d5f44f33825a36c9629587026ef45d0ee
Alexander Levin <alevin.ru@gmail.com>
2018-09-24 07:45:25 +0300

    exceptionHandler debug err stack

--- src/index.js
+++ src/index.js
@@ -20,8 +20,8 @@ async function exceptionHandler(ctx, next) {
 
   try {
     await next();
-  } catch ({ name, message }) {
-    error('exceptionHandler', name, message);
+  } catch ({ message, stack }) {
+    error('exceptionHandler', stack);
     await ctx.replyWithHTML(`Ошибка: <b>${message}</b>`);
   }
 
