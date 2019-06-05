0d492c60e0d75d9adcdae11f46a72ad89bbbf2e6
Alexander Levin <alevin.ru@gmail.com>
2019-06-05 17:33:00 +0300

    cachedAuth

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,10 @@
 import { roles as getRoles } from 'sistemium-telegram/services/auth';
 import log from 'sistemium-telegram/services/log';
+import { setAsync, getAsync } from 'sistemium-telegram/services/redis';
 
 const { debug, error } = log('api');
+
+const AUTH_EXPIRE = parseInt(process.env.AUTH_EXPIRE, 0) || 300;
 
 export default async function (ctx, next) {
 
@@ -14,7 +17,7 @@ export default async function (ctx, next) {
 
   try {
 
-    const { account, roles } = await getRoles(authorization);
+    const { account, roles } = await cachedAuth(authorization);
 
     debug('authorized:', `"${account.name}"`);
     state.auth = { account, roles };
@@ -25,5 +28,24 @@ export default async function (ctx, next) {
   }
 
   await next();
+
+}
+
+async function cachedAuth(authorization) {
+
+  const cached = await getAsync(authorization);
+
+  if (cached) {
+    return JSON.parse(cached);
+  }
+
+  const auth = await getRoles(authorization);
+
+  if (auth.roles && auth.account) {
+    const saved = await setAsync(authorization, JSON.stringify(auth), 'EX', AUTH_EXPIRE);
+    debug('savedAuth', authorization, saved);
+  }
+
+  return auth;
 
 }
