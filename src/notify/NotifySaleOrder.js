245414238ef81813f6965c6e3053787b31274569
Alexander Levin <alevin.ru@gmail.com>
2019-08-29 17:48:35 +0300

    NotifySaleOrder with SQS

--- src/notify/NotifySaleOrder.js
+++ src/notify/NotifySaleOrder.js
@@ -1,12 +1,15 @@
 import log from 'sistemium-telegram/services/log';
 import { eachSeriesAsync } from 'sistemium-telegram/services/async';
 import bot from 'sistemium-telegram/services/bot';
+import { SQS } from 'aws-sdk';
 
 import Chat from '../models/Chat';
 import SaleOrder from '../models/SaleOrder';
 import * as so from '../services/saleOrders';
 
 const { debug, error } = log('NotifySaleOrder');
+
+const { SQS_MAILER_QUEUE_URL } = process.env;
 
 export default class NotifySaleOrder {
 
@@ -19,8 +22,50 @@ export default class NotifySaleOrder {
     this.watch = SaleOrder.watch()
       .on('change', ({ operationType, fullDocument }) => {
         debug(operationType);
-        return operationType === 'insert' && onCreate(fullDocument).catch(error);
+        return operationType === 'insert' && this.onCreate(fullDocument).catch(error);
       });
+
+    if (SQS_MAILER_QUEUE_URL) {
+      debug('init SQS:', SQS_MAILER_QUEUE_URL);
+      this.sqs = new SQS({ region: 'eu-west-1' });
+    } else {
+      debug('SQS_MAILER_QUEUE_URL is empty');
+    }
+
+  }
+
+  async onCreate(saleOrder) {
+
+    debug('onCreate', saleOrder);
+
+    const msg = so.saleOrderView(saleOrder);
+    const chats = await chatsToNotify();
+
+    debug('onCreate', chats.length);
+
+    await eachSeriesAsync(chats, async chat => {
+      await notify(chat.id, `Поступил новый ${msg}`);
+    });
+
+    await this.sendSQS(saleOrder);
+
+  }
+
+  async sendSQS({ id }) {
+
+    if (!this.sqs) {
+      return;
+    }
+
+    const msg = await this.sqs.sendMessage({
+      DelaySeconds: 0,
+      MessageBody: `vrSaleOrderNew/${id}`,
+      QueueUrl: SQS_MAILER_QUEUE_URL,
+    });
+
+    await msg.send();
+
+    debug('sendSQS', id);
 
   }
 
@@ -28,21 +73,6 @@ export default class NotifySaleOrder {
 
 function chatsToNotify() {
   return Chat.find({ 'setting.notifySaleOrder': true });
-}
-
-async function onCreate(saleOrder) {
-
-  debug('onCreate', saleOrder);
-
-  const msg = so.saleOrderView(saleOrder);
-  const chats = await chatsToNotify();
-
-  debug('onCreate', chats.length);
-
-  await eachSeriesAsync(chats, async chat => {
-    await notify(chat.id, `Поступил новый ${msg}`);
-  });
-
 }
 
 async function notify(userId, msg) {
